0ced871e7718f30a6cb991e0ec1c1a4f8dd148e8
cristivalea <160530571+cristivalea@users.noreply.github.com>
2024-11-29 10:20:46 +0200

    Joc functional pentru doi clienti. Un singur meci

--- server.c
+++ server.c
@@ -3,11 +3,10 @@
 #include <time.h>
 #include "functii.h"
 
-void generare_id(char id[], int lungime){
+
+void generare_id(char id[], int lungime) {
     const char caractere[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     int nr_caractere = sizeof(caractere) - 1;
-
-    srand((unsigned int)time(NULL));
 
     for (int i = 0; i < lungime; i++) {
         id[i] = caractere[rand() % nr_caractere];
@@ -15,43 +14,50 @@ void generare_id(char id[], int lungime){
     id[lungime] = '\0';
 }
 
-void citireClient(CLIENT *c){
+void citireClient(CLIENT *c, char simbol_opus) {
     printf("Doriti sa introduceti numele sau vi se va atribui un id?\n");
     int optiune = 0;
     printf("1. Adaugare nume\n");
     printf("2. Atribuire id\n");
     printf("Introduceti optiunea dorita: ");
-    scanf("%d\n", &optiune);
-    switch(optiune){
-        case 1:
-        {
+    scanf("%d", &optiune);
+
+    switch(optiune) {
+        case 1: {
             printf("Introduceti numele: ");
-            scanf("%s\n", c->nume);
+            scanf("%99s", c->nume);
             break;
         }
-        case 2:
-        {
+        case 2: {
             generare_id(c->nume, 20);
+            printf("ID generat: %s\n", c->nume);
+            break;
+        }
+        default:
+            printf("Optiune este invalida.Se va atribui un ID.\n");
+            generare_id(c->nume, 20);
+            break;
+    }
+
+    printf("Introduceti parola dorita: ");
+    scanf("%99s", c->parola);
+
+    printf("Introduceti simbolul dorit:\n");
+    printf("X\n");
+    printf("0\n");
+    while (1) {
+        scanf(" %c", &c->simbol);
+        if (c->simbol == simbol_opus) {
+            printf("A fost ales deja acest simbol.");
+        } else {
             break;
         }
     }
-    printf("%s\n", c->parola);
 }
 
-
-void citire_mutare_clienti(GAME *g){
-    MUTARE m1;
-    printf("Clientul %s introduce mutarea:\n", g->client1.nume);
-    printf("Prima muatare la pozitia:\n");
-    printf("Introduceti linia: ");
-    scanf("%d\n", &m1.x);
-    printf("Introduceti coloana: ");
-    scanf("%d\n", m1.y);
-}
-
-void initializare_tabla(char tabla[LINII][COLOANE]){
-    for(int i = 0; i < LINII; i++){
-        for(int j = 0; j < COLOANE; j++){
+void initializare_tabla(char tabla[LINII][COLOANE]) {
+    for (int i = 0; i < LINII; i++) {
+        for (int j = 0; j < COLOANE; j++) {
             tabla[i][j] = '_';
         }
     }
@@ -60,9 +66,9 @@ void initializare_tabla(char tabla[LINII][COLOANE]){
 void desenare_tabla(char tabla[LINII][COLOANE]) {
     for (int i = 0; i < LINII; i++) {
         for (int j = 0; j < COLOANE; j++) {
-            printf(" %c ", tabla[i][j]); 
+            printf(" %c ", tabla[i][j]);
             if (j < COLOANE - 1) {
-                printf("|"); 
+                printf("|");
             }
         }
         printf("\n");
@@ -71,7 +77,7 @@ void desenare_tabla(char tabla[LINII][COLOANE]) {
             for (int j = 0; j < COLOANE; j++) {
                 printf("---");
                 if (j < COLOANE - 1) {
-                    printf("+"); 
+                    printf("+");
                 }
             }
             printf("\n");
@@ -79,34 +85,96 @@ void desenare_tabla(char tabla[LINII][COLOANE]) {
     }
 }
 
-int verigficare_tabla_plina(char tabla[LINII][COLOANE]){
-    BOOLEAN verificare = FALSE;
-    int contor = 0;
-    for(int i = 0; i < LINII; i++){
-        for(int j = 0; j < COLOANE; j++){
-            if(tabla[i][j] == '_'){
-                contor++;
+int verificare_tabla_plina(char tabla[LINII][COLOANE]) {
+    for (int i = 0; i < LINII; i++) {
+        for (int j = 0; j < COLOANE; j++) {
+            if (tabla[i][j] == '_') {
+                return FALSE;
             }
         }
     }
-    if(contor == 9){
-        verificare = TRUE;
-    }
-    return verificare;
+    return TRUE;
 }
 
+int verificare_castigator(char tabla[LINII][COLOANE], char simbol) {
+    for (int i = 0; i < LINII; i++) {
+        if (tabla[i][0] == simbol && tabla[i][1] == simbol && tabla[i][2] == simbol) {
+            return TRUE;
+        }
+    }
+    for (int j = 0; j < COLOANE; j++) {
+        if (tabla[0][j] == simbol && tabla[1][j] == simbol && tabla[2][j] == simbol) {
+            return TRUE;
+        }
+    }
+    if (tabla[0][0] == simbol && tabla[1][1] == simbol && tabla[2][2] == simbol) {
+        return TRUE;
+    }
+    if (tabla[0][2] == simbol && tabla[1][1] == simbol && tabla[2][0] == simbol) {
+        return TRUE;
+    }
+    return FALSE;
+}
 
-void joc(GAME game){
-    initializare_tabla(game.tabla);
-    //desenare_tabla(game.tabla);
-    while(verigficare_tabla_plina(game.tabla) == FALSE){
+void citire_mutare_clienti(GAME *g, char simbol) {
+    MUTARE m1;
+    printf("Introduceti mutarea (%c):\n", simbol);
+    printf("Introduceti linia: ");
+    scanf("%d", &m1.x);
+    printf("Introduceti coloana: ");
+    scanf("%d", &m1.y);
 
+    if (m1.x < 0 || m1.x >= LINII || m1.y < 0 || m1.y >= COLOANE) {
+        printf("Mutare este invalida. Introduceti alta mutare.\n");
+        citire_mutare_clienti(g, simbol);
+    } else if (g->tabla[m1.x][m1.y] != '_') {
+        printf("Pozitia aleasa este ocupata. Alegeti alta pozitie.\n");
+        citire_mutare_clienti(g, simbol);
+    } else {
+        g->tabla[m1.x][m1.y] = simbol;
     }
 }
 
-int main(void){
+void joc(GAME *game) {
+    initializare_tabla(game->tabla);
+    desenare_tabla(game->tabla);
+
+    int mutari = 0;
+    while (verificare_tabla_plina(game->tabla) == FALSE) {
+        if (mutari % 2 == 0) {
+            printf("Tura clientului %s (%c):\n", game->client1.nume, game->client1.simbol);
+            citire_mutare_clienti(game, game->client1.simbol);
+            desenare_tabla(game->tabla);
+            if (verificare_castigator(game->tabla, game->client1.simbol)) {
+                printf("Clientul %s a castigat!\n", game->client1.nume);
+                return;
+            }
+        } else {
+            printf("Tura clientului %s (%c):\n", game->client2.nume, game->client2.simbol);
+            citire_mutare_clienti(game, game->client2.simbol);
+            desenare_tabla(game->tabla);
+            if (verificare_castigator(game->tabla, game->client2.simbol)) {
+                printf("Clientul %s a castigat!\n", game->client2.nume);
+                return;
+            }
+        }
+        mutari++;
+    }
+
+    printf("Tabla este plină. Egalitate intre jucatori.\n");
+}
+
+int main(void) {
+    srand((unsigned int)time(NULL)); 
+
     GAME game;
-    joc(game);
+
+    printf("Introduceti detalii pentru clientul 1:\n");
+    citireClient(&game.client1, '\0');
+    printf("Introduceti detalii pentru clientul 2:\n");
+    citireClient(&game.client2, game.client1.simbol);
+
+    joc(&game);
+
     return 0;
 }
-
