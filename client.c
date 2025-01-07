289168bf421fdd00fe44089efd271dc1b6982f16
Cosminvlc03 <163720283+Cosminvlc03@users.noreply.github.com>
2025-01-07 20:01:00 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -1,208 +1,81 @@
+// client.c
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <ctype.h>
-#include <sys/select.h>
 
 #define PORT 12345
-#define MAX_NAME_LENGTH 20
-#define BUFFER_SIZE 1024
-#define DEFAULT_SERVER "127.0.0.1"
 
-void clearInputBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
-
-void handle_error(const char* message, int sockfd) {
-    perror(message);
-    if (sockfd >= 0) {
-        close(sockfd);
-    }
-    exit(1);
-}
-
-void safeGetString(char *buffer, int maxLength) {
-    if (fgets(buffer, maxLength, stdin) != NULL) {
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len-1] == '\n') {
-            buffer[len-1] = '\0';
-        } else {
-            clearInputBuffer();
-        }
-    }
-}
-
-void makeMove(int sockfd) {
+void joacaMutare(int sockfd) {
     int row, col;
-    char move[10];
-    char input[32];
+    char mutare[10];
 
-    while (1) {
-        printf("\nIntroduceti mutarea (linia si coloana 0-2, ex: '1 2'): ");
-        safeGetString(input, sizeof(input));
+    printf("Introduceti linia si coloana (ex: 1 2): ");
+    scanf("%d %d", &row, &col);
 
-        if (sscanf(input, "%d %d", &row, &col) == 2) {
-            if (row >= 0 && row < 3 && col >= 0 && col < 3) {
-                sprintf(move, "%d %d", row, col);
-                write(sockfd, move, strlen(move));
-                break;
-            }
-        }
-        printf("Mutare invalida! Va rugam introduceti doua numere intre 0 si 2.\n");
-    }
+    sprintf(mutare, "%d %d", row, col);
+    send(sockfd, mutare, strlen(mutare), 0);
 }
 
-void handleGameMessages(int sockfd) {
-    char buffer[BUFFER_SIZE];
-    int game_active = 1;
-    fd_set readfds;
-    struct timeval tv;
-    
-    while (game_active) {
-        FD_ZERO(&readfds);
-        FD_SET(sockfd, &readfds);
-        FD_SET(STDIN_FILENO, &readfds);
-        
-        tv.tv_sec = 1;
-        tv.tv_usec = 0;
-        
-        int activity = select(sockfd + 1, &readfds, NULL, NULL, &tv);
-        if (activity < 0) {
-            perror("Select error");
-            break;
-        }
-        
-        if (activity > 0) {
-            if (FD_ISSET(sockfd, &readfds)) {
-                memset(buffer, 0, sizeof(buffer));
-                int bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
-                
-                if (bytes_received <= 0) {
-                    printf("\nConexiunea cu serverul a fost intrerupta.\n");
-                    break;
-                }
-                
-                buffer[bytes_received] = '\0';
-                
-                if (strcmp(buffer, "TURN") == 0) {
-                    printf("\n>>> Este randul tau! <<<\n");
-                    makeMove(sockfd);
-                }
-                else if (strcmp(buffer, "REPLAY") == 0) {
-                    printf("\nDoriti sa jucati din nou? (da/nu): ");
-                    char response[10];
-                    safeGetString(response, sizeof(response));
-                    write(sockfd, response, strlen(response));
-                    
-                    if (strcasecmp(response, "nu") == 0) {
-                        printf("\nMultumim pentru joc! La revedere!\n");
-                        game_active = 0;
-                    }
-                }
-                else {
-                    printf("%s", buffer);
-                }
-            }
-        }
-    }
-}
-
-int connectToServer(const char* address) {
+int main() {
     int sockfd;
     struct sockaddr_in serv_addr;
-    struct timeval timeout;
-    
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        handle_error("Socket creation failed", -1);
-    }
-    
-    timeout.tv_sec = 30;  
-    timeout.tv_usec = 0;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
-        handle_error("Setsockopt failed", sockfd);
-    }
-    
-    memset(&serv_addr, 0, sizeof(serv_addr));
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    
-    if (inet_pton(AF_INET, address, &serv_addr.sin_addr) <= 0) {
-        handle_error("Invalid address", sockfd);
-    }
-    
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        handle_error("Connection failed", sockfd);
-    }
-    
-    return sockfd;
-}
-
-int main(int argc, char *argv[]) {
-    const char* server_address = DEFAULT_SERVER;
-    
-    if (argc == 2) {
-        server_address = argv[1];
-    }
-
-    system("clear");
-    printf("\n=== Bine ati venit la X si 0 ===\n\n");
-
-    int sockfd = connectToServer(server_address);
-    if (sockfd < 0) {
+        perror("Connect failed");
         return 1;
     }
 
-    char buffer[BUFFER_SIZE];
+    char buffer[1024];
+    printf("Conectat la server. Asteptati...");
+
+ printf("1. Create new game\n");
+    printf("2. Join game by ID\n");
+    printf("3. Join random game\n");
+    printf("Choice: ");
     
-    printf("Introduceti numele dvs. (max %d caractere): ", MAX_NAME_LENGTH - 1);
-    safeGetString(buffer, MAX_NAME_LENGTH);
-    if (strlen(buffer) == 0 || strspn(buffer, " \t\n\r") == strlen(buffer)) {
-        strcpy(buffer, "Player");
+    int choice;
+    scanf("%d", &choice);
+
+    switch(choice) {
+        case 1:
+            send(sockfd, "NEW", 3, 0);
+            recv(sockfd, buffer, sizeof(buffer), 0);
+            printf("Game created! ID: %s\n", buffer);
+            break;
+        case 2:
+            printf("Enter game ID: ");
+            scanf("%s", buffer);
+            send(sockfd, buffer, strlen(buffer), 0);
+            break;
+        case 3:
+            send(sockfd, "RANDOM", 6, 0);
+            break;
     }
-    write(sockfd, buffer, strlen(buffer));
-    printf("Nume inregistrat: %s\n", buffer);
 
     while (1) {
-        printf("\nDoriti sa:\n");
-        printf("1. Creati un joc nou\n");
-        printf("2. Va alaturati unui joc existent\n");
-        printf("Alegerea dvs. (1/2): ");
-        
-        char choice[10];
-        safeGetString(choice, sizeof(choice));
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
 
-        if (choice[0] == '1') {
-            write(sockfd, "CREATI", 6);
-            
-            memset(buffer, 0, sizeof(buffer));
-            int received = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
-            if (received > 0) {
-                buffer[received] = '\0';
-                printf("\nJoc creat! ID-ul jocului: %s\n", buffer);
-                printf("Asteptati al doilea jucator...\n\n");
-                break;
-            } else {
-                printf("Eroare la crearea jocului.\n");
-                close(sockfd);
-                return 1;
-            }
-        }
-        else if (choice[0] == '2') {
-            printf("\nIntroduceti ID-ul jocului: ");
-            safeGetString(buffer, sizeof(buffer));
-            write(sockfd, buffer, strlen(buffer));
-            printf("\nSe incearca conectarea la joc...\n\n");
+        if (bytes_received <= 0) {
+            printf("Conexiunea s-a inchis.\n");
             break;
         }
-        else {
-            printf("\nOptiune invalida! Va rugam alegeti 1 sau 2.\n");
+
+        buffer[bytes_received] = '\0';
+        printf("%s\n", buffer);
+
+        if (strcmp(buffer, "TURN") == 0) {
+            joacaMutare(sockfd);
         }
     }
-
-    handleGameMessages(sockfd);
 
     close(sockfd);
     return 0;
--- server.c
+++ server.c
@@ -4,53 +4,28 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-#include <time.h>
-#include <errno.h>
-#include <signal.h>
-#include <sys/select.h>
 
 #define PORT 12345
-#define MAX_CLIENTS 10
-#define ID_LENGTH 6
-#define MAX_NAME_LENGTH 20
-
-volatile sig_atomic_t server_running = 1;
+#define MAX_CLIENTS 2
 
 pthread_mutex_t game_mutex = PTHREAD_MUTEX_INITIALIZER;
+char board[3][3];
+int clients[MAX_CLIENTS];
+int current_turn = 0;
 
 typedef struct {
-    char game_id[ID_LENGTH + 1];
+    char id[7];
     int clients[2];
-    char player_names[2][MAX_NAME_LENGTH];
-    int client_count;
     char board[3][3];
     int current_turn;
-    int moves_count;
-    int game_running;
+    int active;
 } Game;
 
-Game games[MAX_CLIENTS];
+#define MAX_GAMES 10
+Game games[MAX_GAMES];
 int game_count = 0;
 
-void handle_signal(int sig) {
-    server_running = 0;
-}
-
-void cleanup_server(int server_fd) {
-    for (int i = 0; i < game_count; i++) {
-        for (int j = 0; j < games[i].client_count; j++) {
-            if (games[i].clients[j] > 0) {
-                close(games[i].clients[j]);
-            }
-        }
-    }
-    if (server_fd > 0) {
-        close(server_fd);
-    }
-    pthread_mutex_destroy(&game_mutex);
-}
-
-void initialize_board(char board[3][3]) {
+void initialize_board() {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             board[i][j] = ' ';
@@ -58,293 +33,164 @@ void initialize_board(char board[3][3]) {
     }
 }
 
-void generate_id(char id[], int length) {
-    const char characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    int num_characters = sizeof(characters) - 1;
-    srand((unsigned int)time(NULL));
-    for (int i = 0; i < length; i++) {
-        id[i] = characters[rand() % num_characters];
+void generate_game_id(char *id) {
+    const char chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for(int i = 0; i < 6; i++) {
+        id[i] = chars[rand() % strlen(chars)];
     }
-    id[length] = '\0';
+    id[6] = '\0';
 }
 
-void print_board(char *buffer, char board[3][3]) {
-    sprintf(buffer, "\n %c | %c | %c \n---+---+---\n %c | %c | %c \n---+---+---\n %c | %c | %c \n",
-            board[0][0], board[0][1], board[0][2],
-            board[1][0], board[1][1], board[1][2],
-            board[2][0], board[2][1], board[2][2]);
-}
-
-int check_win(char board[3][3], char symbol) {
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol)
-            return 1;
-    }
-    for (int i = 0; i < 3; i++) {
-        if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol)
-            return 1;
-    }
-    if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol)
-        return 1;
-    if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol)
-        return 1;
-    
-    return 0;
-}
-
-Game* find_game_by_id(const char *game_id) {
-    for (int i = 0; i < game_count; i++) {
-        if (strcmp(games[i].game_id, game_id) == 0) {
+Game* find_game(const char* id) {
+    for(int i = 0; i < game_count; i++) {
+        if(strcmp(games[i].id, id) == 0 && games[i].active) {
             return &games[i];
         }
     }
     return NULL;
 }
 
-void reset_game(Game *game) {
-    initialize_board(game->board);
+Game* create_game() {
+    if(game_count >= MAX_GAMES) return NULL;
+    Game* game = &games[game_count++];
+    generate_game_id(game->id);
+    game->clients[0] = -1;
+    game->clients[1] = -1;
     game->current_turn = 0;
-    game->moves_count = 0;
-    game->game_running = 1;
-    
-    char message[1024];
-    sprintf(message, "Joc nou inceput! Este randul lui %s (X)\n", game->player_names[0]);
-    write(game->clients[0], message, strlen(message));
-    write(game->clients[1], message, strlen(message));
-    write(game->clients[0], "TURN", 4);
+    game->active = 1;
+    initialize_board(game->board);
+    return game;
 }
 
-void notify_game_start(Game *game) {
-    char message[1024];
-    sprintf(message, "Jocul a inceput! %s (X) vs %s (O)\n", 
-            game->player_names[0], game->player_names[1]);
-    write(game->clients[0], message, strlen(message));
-    write(game->clients[1], message, strlen(message));
-    write(game->clients[0], "TURN", 4);
-}
-
-void handle_disconnection(Game *game, int client_socket) {
-    char message[1024] = "Un jucator s-a deconectat. Jocul se inchide.\n";
-    for (int i = 0; i < game->client_count; i++) {
-        if (game->clients[i] != client_socket) {
-            write(game->clients[i], message, strlen(message));
-            close(game->clients[i]);
+Game* find_random_game() {
+    for(int i = 0; i < game_count; i++) {
+        if(games[i].active && games[i].clients[1] == -1) {
+            return &games[i];
         }
     }
-    game->game_running = 0;
+    return NULL;
 }
 
-void process_turn(Game *game, int client_socket) {
-    char buffer[1024];
-    int current_player = (game->clients[0] == client_socket) ? 0 : 1;
-    char symbol = (current_player == 0) ? 'X' : 'O';
-    
-    while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        int received = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
-        
-        if (received <= 0) {
-            handle_disconnection(game, client_socket);
-            return;
-        }
-        
-        if (strcmp(buffer, "restart") == 0) {
-            reset_game(game);
-            return;
-        }
-        
-        int row, col;
-        if (sscanf(buffer, "%d %d", &row, &col) == 2) {
-            if (row >= 0 && row < 3 && col >= 0 && col < 3 && game->board[row][col] == ' ') {
-                game->board[row][col] = symbol;
-                game->moves_count++;
-                
-                char board_display[1024];
-                print_board(board_display, game->board);
-                write(game->clients[0], board_display, strlen(board_display));
-                write(game->clients[1], board_display, strlen(board_display));
-                
-                if (check_win(game->board, symbol)) {
-                    char win_message[1024];
-                    sprintf(win_message, "Jucatorul %s a castigat!\n", game->player_names[current_player]);
-                    write(game->clients[0], win_message, strlen(win_message));
-                    write(game->clients[1], win_message, strlen(win_message));
-                    write(game->clients[0], "REPLAY", 6);
-                    write(game->clients[1], "REPLAY", 6);
-                    return;
-                } else if (game->moves_count >= 9) {
-                    write(game->clients[0], "Egalitate!\n", 11);
-                    write(game->clients[1], "Egalitate!\n", 11);
-                    
-                    write(game->clients[0], "REPLAY", 6);
-                    write(game->clients[1], "REPLAY", 6);
-                    return;
-                }
-                
-                game->current_turn = 1 - game->current_turn;
-                write(game->clients[game->current_turn], "TURN", 4);
-                return;
-            } else {
-                write(client_socket, "Mutare invalida. Incercati din nou.\n", 400);
-            }
-        } else {
-            write(client_socket, "Format invalid. Introduceti linia si coloana (ex: 1 2).\n", 600);
-        }
+void print_board(char *buffer) {
+    sprintf(buffer, "\n %c | %c | %c \n---+---+---\n %c | %c | %c \n---+---+---\n %c | %c | %c \n",
+            board[0][0], board[0][1], board[0][2],
+            board[1][0], board[1][1], board[1][2],
+            board[2][0], board[2][1], board[2][2]);
+}
+
+int check_winner() {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') return 1;
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') return 1;
     }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') return 1;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') return 1;
+    return 0;
 }
 
 void *client_handler(void *arg) {
+    int row, col;
     int client_socket = *(int *)arg;
     free(arg);
-    
     char buffer[1024];
-    Game *game = NULL;
+    Game* game = NULL;
+
+    recv(client_socket, buffer, sizeof(buffer) - 1, 0);
     
-    memset(buffer, 0, sizeof(buffer));
-    if (read(client_socket, buffer, sizeof(buffer) - 1) <= 0) {
-        close(client_socket);
-        return NULL;
-    }
-    char player_name[MAX_NAME_LENGTH];
-    strncpy(player_name, buffer, MAX_NAME_LENGTH - 1);
-    player_name[MAX_NAME_LENGTH - 1] = '\0';
-    printf("Player connected: %s\n", player_name);
-    
-    memset(buffer, 0, sizeof(buffer));
-    if (read(client_socket, buffer, sizeof(buffer) - 1) <= 0) {
-        close(client_socket);
-        return NULL;
-    }
-    
-    if (strcmp(buffer, "CREATI") == 0) {
-        pthread_mutex_lock(&game_mutex);
-        game = &games[game_count++];
-        generate_id(game->game_id, ID_LENGTH);
-        game->clients[0] = client_socket;
-        strncpy(game->player_names[0], player_name, MAX_NAME_LENGTH - 1);
-        game->client_count = 1;
-        initialize_board(game->board);
-        game->current_turn = 0;
-        game->moves_count = 0;
-        game->game_running = 1;
-        printf("Game created with ID: %s\n", game->game_id);
-        write(client_socket, game->game_id, strlen(game->game_id));
-        pthread_mutex_unlock(&game_mutex);
-        
+    pthread_mutex_lock(&game_mutex);
+    if(strcmp(buffer, "NEW") == 0) {
+        game = create_game();
+        if(game) {
+            game->clients[0] = client_socket;
+            send(client_socket, game->id, strlen(game->id), 0);
+        }
+    } else if(strcmp(buffer, "RANDOM") == 0) {
+        game = find_random_game();
+        if(!game) game = create_game();
+        if(game->clients[0] == -1) game->clients[0] = client_socket;
+        else game->clients[1] = client_socket;
     } else {
-        pthread_mutex_lock(&game_mutex);
-        game = find_game_by_id(buffer);
-        if (game && game->client_count < 2) {
+        game = find_game(buffer);
+        if(game && game->clients[1] == -1) {
             game->clients[1] = client_socket;
-            strncpy(game->player_names[1], player_name, MAX_NAME_LENGTH - 1);
-            game->client_count++;
-            notify_game_start(game);
-        } else {
-            write(client_socket, "Jocul este deja complet sau nu exista.\n", 39);
+        }
+    }
+    pthread_mutex_unlock(&game_mutex);
+
+    if(!game || (game->clients[0] != client_socket && game->clients[1] != client_socket)) {
+        send(client_socket, "ERROR", 5, 0);
+        close(client_socket);
+        return NULL;
+    }
+
+    while(game->clients[1] == -1) {
+        sleep(1);
+    }
+
+    while (1) {
+        pthread_mutex_lock(&game_mutex);
+        if (client_socket == clients[current_turn]) {
+            send(client_socket, "TURN", 4, 0);
             pthread_mutex_unlock(&game_mutex);
-            close(client_socket);
-            return NULL;
+
+            memset(buffer, 0, sizeof(buffer));
+            recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+            sscanf(buffer, "%d %d", &row, &col);
+
+            pthread_mutex_lock(&game_mutex);
+            if (board[row][col] == ' ') {
+                board[row][col] = (current_turn == 0) ? 'X' : 'O';
+                current_turn = 1 - current_turn;
+            }
+            print_board(buffer);
+            send(clients[0], buffer, strlen(buffer), 0);
+            send(clients[1], buffer, strlen(buffer), 0);
+
+            if (check_winner()) {
+                strcat(buffer, "\nA castigat unul dintre jucatori!\n");
+                send(clients[0], buffer, strlen(buffer), 0);
+                send(clients[1], buffer, strlen(buffer), 0);
+                break;
+            }
         }
         pthread_mutex_unlock(&game_mutex);
     }
-    
-    while (game && game->game_running) {
-        pthread_mutex_lock(&game_mutex);
-        if (client_socket == game->clients[game->current_turn]) {
-            pthread_mutex_unlock(&game_mutex);
-            process_turn(game, client_socket);
-        } else {
-            pthread_mutex_unlock(&game_mutex);
-            usleep(100000);
-        }
-    }
-    
+
     close(client_socket);
     return NULL;
 }
 
 int main() {
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = handle_signal;
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
-    
     int server_fd, client_socket;
     struct sockaddr_in address;
     socklen_t addr_len = sizeof(address);
-    
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation failed");
-        return 1;
-    }
-    
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("setsockopt failed");
-        close(server_fd);
-        return 1;
-    }
-    
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-    
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        close(server_fd);
-        return 1;
-    }
-    
-    if (listen(server_fd, MAX_CLIENTS) < 0) {
-        perror("Listen failed");
-        close(server_fd);
-        return 1;
-    }
-    
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, MAX_CLIENTS);
+
     printf("Server pornit pe portul %d\n", PORT);
-    
-    while (server_running) {
-        fd_set readfds;
-        struct timeval tv;
-        FD_ZERO(&readfds);
-        FD_SET(server_fd, &readfds);
-        tv.tv_sec = 1;
-        tv.tv_usec = 0;
-        
-        int activity = select(server_fd + 1, &readfds, NULL, NULL, &tv);
-        if (activity < 0 && errno != EINTR) {
-            perror("Select error");
-            break;
-        }
-        
-        if (activity > 0 && FD_ISSET(server_fd, &readfds)) {
-            if ((client_socket = accept(server_fd, (struct sockaddr *)&address, &addr_len)) < 0) {
-                perror("Accept failed");
-                continue;
-            }
-            
-            printf("Client conectat.\n");
-            
-            int *new_sock = malloc(sizeof(int));
-            if (new_sock == NULL) {
-                perror("Failed to allocate memory");
-                close(client_socket);
-                continue;
-            }
-            
-            *new_sock = client_socket;
-            pthread_t thread_id;
-            if (pthread_create(&thread_id, NULL, client_handler, (void *)new_sock) != 0) {
-                perror("Could not create thread");
-                free(new_sock);
-                close(client_socket);
-                continue;
-            }
-            pthread_detach(thread_id);
-        }
+    initialize_board();
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        client_socket = accept(server_fd, (struct sockaddr *)&address, &addr_len);
+        printf("Client conectat.\n");
+        clients[i] = client_socket;
+
+        pthread_t thread_id;
+        int *new_sock = malloc(sizeof(int));
+        *new_sock = client_socket;
+        pthread_create(&thread_id, NULL, client_handler, (void *)new_sock);
     }
-    
-    cleanup_server(server_fd);
+
+    while (1) {
+        sleep(1);
+    }
+
+    close(server_fd);
     return 0;
 }
